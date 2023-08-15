52aff6ca83f9118cf270ebfa8c08d62ee32db934
jingtao1 <604181915@qq.com>
2023-08-15 23:19:35 +0800

    配置中心服务端配置完成

--- book-service/src/main/java/com/test/controller/BookController.java
+++ book-service/src/main/java/com/test/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class BookController {
@@ -15,7 +16,9 @@ public class BookController {
     IBookService bookService;
 
     @RequestMapping("/book/{bid}")
-    Book findBookById(@PathVariable("bid") int bid){
+    Book findBookById(@PathVariable("bid") int bid, HttpServletRequest request){
+        String test = request.getHeader("Tests");
+        System.out.println(test);
         return bookService.getBookById(bid);
     }
 }
